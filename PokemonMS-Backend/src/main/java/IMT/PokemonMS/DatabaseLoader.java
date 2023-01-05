3346abd44c73dcc8ccb6bf47e7218230ec8ca0c0
Eliot <eliotmasset@gmail.com>
2023-01-05 20:14:12 +0100

    [FEATURE] Add sqlite database connection

--- PokemonMS-Backend/src/main/java/IMT/PokemonMS/DatabaseLoader.java
+++ PokemonMS-Backend/src/main/java/IMT/PokemonMS/DatabaseLoader.java
@@ -2,12 +2,44 @@ package IMT.PokemonMS;
 
 import org.springframework.stereotype.Component;
 import org.springframework.boot.CommandLineRunner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Component
 public class DatabaseLoader implements CommandLineRunner {
 
+	private static Connection conn = null;
+
 	@Override
 	public void run(String... strings) throws Exception {
-		// this.repository.save(new PokemonType("Pikachu"));
+		connect();
+	}
+
+   public static void connect() {
+		try {
+			String url = "jdbc:sqlite:pokemonMS.db";
+			//String url = "jdbc:sqlite:/mnt/c/pokemonMS.db"; // for wsl development
+			conn = DriverManager.getConnection(url);
+
+			System.out.println("Connection to SQLite has been established.");
+
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static Connection getConnection() {
+		return conn;
+	}
+
+	public static void closeConnection() {
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 }
