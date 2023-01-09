b5cb16484273868fba2175c3054373885aaa727c
eliot.masset <eliotmasset@gmail.com>
2023-01-09 15:42:24 +0100

    [UPDATE] backend architecture

---
+++ PokemonMS-Backend/Tower/src/main/java/IMT/PokemonMS/Tower.java
@@ -0,0 +1,22 @@
+package IMT.PokemonMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Oeuf {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oeuf.class, args);
+	}
+
+	@GetMapping("/test")
+	public Boolean test() {
+		return true;
+	}
+
+}
