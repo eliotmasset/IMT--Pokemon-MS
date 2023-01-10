ea1622183d9058453ee3471216ccaed9ccea5749
eliot.masset <eliotmasset@gmail.com>
2023-01-10 16:17:51 +0100

    [UPDATE] animate all curent window openning

--- PokemonMS-Frontend/src/Settings/SettingsApp.js
+++ PokemonMS-Frontend/src/Settings/SettingsApp.js
@@ -2,9 +2,23 @@ import './SettingsApp.css';
 import React from 'react';
 
 class SettingsApp extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDisplayed: false
+    }
+  }
+
+  setIsDisplayed(isDisplayed) {
+    this.setState({isDisplayed: isDisplayed});
+  }
+
   render() {
+    let className = "";
+    if(this.state.isDisplayed) className = "displayed";
     return (
-      <div id="SettingsApp">
+      <div id="SettingsApp" className={className} onClick={() => this.setIsDisplayed(true)}>
         <div id="backdrop">
             <img class="littleIco" src="/HomeIco/SettingsIco.png"/>
         </div>
--- PokemonMS-Frontend/src/Team/TeamApp.js
+++ PokemonMS-Frontend/src/Team/TeamApp.js
@@ -2,9 +2,23 @@ import './TeamApp.css';
 import React from 'react';
 
 class TeamApp extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        isDisplayed: false
+      }
+    }
+  
+    setIsDisplayed(isDisplayed) {
+      this.setState({isDisplayed: isDisplayed});
+    }
+  
     render() {
+      let className = "";
+      if(this.state.isDisplayed) className = "displayed";
         return (
-            <div id="TeamApp">
+            <div id="TeamApp" className={className} onClick={() => this.setIsDisplayed(true)}>
                 <div id="backdrop">
                     <img class="littleIco" src="/HomeIco/TeamIco.png"/>
                 </div>
