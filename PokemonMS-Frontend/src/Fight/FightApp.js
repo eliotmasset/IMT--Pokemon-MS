e55bdf6ef6b733df2c410d767525062dd3950c86
eliot.masset <eliotmasset@gmail.com>
2023-01-10 14:25:01 +0100

    [UPDATE] animation window fight

--- PokemonMS-Frontend/src/Fight/FightApp.js
+++ PokemonMS-Frontend/src/Fight/FightApp.js
@@ -2,9 +2,23 @@ import './FightApp.css';
 import React from 'react';
 
 class FightApp extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDisplayed: false
+    }
+  }
+
+  setIsDisplayed(isDisplayed) {
+    this.setState({isDisplayed: isDisplayed});
+  }
+
   render() {
+    let className = "";
+    if(this.state.isDisplayed) className = "displayed";
     return (
-      <div id="FightApp">
+      <div id="FightApp" className={className} onClick={() => this.setIsDisplayed(true)}>
 
         <div id="backdrop">
             <h2> COMBATS </h2>
@@ -18,7 +32,4 @@ class FightApp extends React.Component {
     );
   }
 }
-
-
-
 export default FightApp;
