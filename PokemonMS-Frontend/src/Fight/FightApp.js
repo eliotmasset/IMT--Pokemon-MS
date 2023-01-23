cb679a9ae7efaeb227302d94a2bacfccbd2639c8
Boubou0 <78027938+Boubou0@users.noreply.github.com>
2023-01-23 17:32:06 +0100

    [UPDATE] FightApp component + css

--- PokemonMS-Frontend/src/Fight/FightApp.js
+++ PokemonMS-Frontend/src/Fight/FightApp.js
@@ -1,5 +1,6 @@
 import './FightApp.css';
 import React from 'react';
+import BackButtonComponent from "../components/BackButtonComponent";
 
 class FightApp extends React.Component {
 
@@ -27,6 +28,17 @@ class FightApp extends React.Component {
 
 
         <div id="FightWindow">
+            <div id="blackOpacity">
+                <div className="towerLvl">23
+                    <h1 className="etage">Etage</h1>
+                </div>
+                <button className="towerFight">
+                    <img className="hide-on-hover" src={'/towerMenuImg/black_swords.png'}/>
+                    <img className="show-on-hover" src={'/towerMenuImg/white_swords.png'}/>
+                    <h1 className="startFight">Commencer le combat</h1>
+                </button>
+                <BackButtonComponent onClick={() => this.setIsDisplayed(false)}></BackButtonComponent>
+            </div>
         </div>
 
       </div>
